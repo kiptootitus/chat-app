13954f79c675e5d4b5933721e90dfa2c2e3b66ae
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-02-21 17:33:03 +0300

    upd

---
+++ chatroom/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import chatroom
+
+def members(request):
+  chatrooms = chatroom.objects.all().values()
+  template = loader.get_template('mysite.html')
+  context = {
+    'chatroom': chatrooms,
+  }
+  return HttpResponse(template.render(context, request))
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
