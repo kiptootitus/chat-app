cb19aa7e83e0ea89cbf003f46fb39aa6ba81b77a
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-02-24 21:31:23 +0300

    views models urls

---
+++ mychatapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('friends/<str:pk>/', views.detail, name='detail')
+    
+]
--- mychatapp/views.py
+++ mychatapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'mychatapp/index.html')
+
+def detail(request, room_name):
+    context = { }
+    return render(request, 'mychatapp/detail.html', context)
